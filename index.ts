8046d0929ddb56ddc2313ea1b921c28f0da33d40
Safiyya Babio <safiyya.babio@gmail.com>
2019-02-13 18:04:05 +0200

    typo

--- index.ts
+++ index.ts
@@ -2,4 +2,4 @@ import { FullstoryModule } from './src/fullstory.module'
 import { Fullstory } from './src/fullstory/fullstory'
 import { FullstoryConfig } from './src/shared/fullstory-config'
 
-export (FullstoryModule, Fullstory, FullstoryConfig}
+export {FullstoryModule, Fullstory, FullstoryConfig}
